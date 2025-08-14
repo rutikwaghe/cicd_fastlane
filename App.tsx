6cc3f7ef1cfa870e48a5d8ba40ef0ea41acf2694
rutikwaghe <rutik.w@mobilestyx.co.in>
2025-08-14 15:15:54 +0530

    test

--- App.tsx
+++ App.tsx
@@ -1,6 +1,4 @@
-// import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
-import { withStallion } from 'react-native-stallion';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -8,8 +6,7 @@ function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Text>Hello</Text>
-      {/* <NewAppScreen templateFileName="App.tsx" /> */}
+      <Text style={{ color: "#000" }}>Hello world</Text>
     </View>
   );
 }
@@ -23,5 +20,51 @@ const styles = StyleSheet.create({
   },
 });
 
-export default withStallion(App)
+export default App
+
+
+
+
+
+
+
+
+
+// override fun getJSBundleFile(): String? {
+//         return Stallion.getJSBundleFile(applicationContext)
+//       }
+
+{/* <resources>
+    <string name="app_name">TestApp</string>
+    <string name="StallionProjectId">6899bcdff0de5106a2e8a6f0</string>
+    <string name="StallionAppToken">spb_lq36HpfUICDkiQ1sRycmwdPjOxPYVogb7QbFxThH5b</string>
+</resources> */}
+
+//  "react-native-stallion": "^2.2.0"
+// "stallion-cli": "^2.3.1",
+
+// import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+// import { withStallion } from 'react-native-stallion';
+
+// function App() {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+//       <Text>Hello</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "white",
+//     justifyContent: "center",
+//     alignItems: "center",
+//   },
+// });
+
+// export default withStallion(App)
 
